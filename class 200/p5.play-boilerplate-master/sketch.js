4beec513e86384568dbf714d615dcd81621d098d
CODERANANT2 <72502773+CODERANANT2@users.noreply.github.com>
2020-10-26 11:46:46 +0530

    Add files via upload

--- class 200/p5.play-boilerplate-master/sketch.js
+++ class 200/p5.play-boilerplate-master/sketch.js
@@ -7,7 +7,7 @@ function setup() {
   speed=random(55,90);
   weight=random(400,1500);
   car=createSprite(50,200,50,50);
-  wall=createSprite(1500,200,60,height/2);
+  wall=createSprite(1500,200,60,120);
   car.velocityX=2;
 
   
